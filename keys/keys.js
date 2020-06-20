6579241998e9362820624e574498f0b91c24056b
JAgrit20 <jagritacharya2020@gmail.com>
2020-06-20 11:30:23 +0530

    keys updated

--- keys/keys.js
+++ keys/keys.js
@@ -1,2 +1,4 @@
 module.exports = {
+serverConnectionUrl: 'mongodb+srv://jagrti:JHA8875091601@cluster0-ul1ff.mongodb.net/login_details?retryWrites=true&w=majority',
+JWT_SECRET: "DhanYnsb09b14bIigwL9gs6b52CcvZxQ"
 }
