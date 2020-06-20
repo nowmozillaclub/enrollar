e53dfe4c947d5358337381a332d86b16df478f23
BlackHatGhost <yashdineshgoyal@gmail.com>
2020-06-20 12:00:02 +0530

    added Sign in page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,65 @@
-import React, { Fragment } from 'react';
-
+import React, { Fragment } from "react";
 
 // import "./App.css";
 
-import {
-  BrowserRouter,
-  Route,
-  Switch
-} from 'react-router-dom';
-import HomePage  from './pages/HomePage';
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SingnIn from "./pages/SignIn";
 
 function App() {
   return (
     <Fragment>
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' component={HomePage} />
+          {/* <Route exact path="/" component={HomePage} /> */}
+          <div className="App">
+            <div className="App__Aside"></div>
+            <div className="App__Form">
+              <div className="PageSwitcher">
+                <NavLink
+                  to="/sign-in"
+                  activeClassName="PageSwitcher__Item--Active"
+                  className="PageSwitcher__Item"
+                >
+                  Sign In
+                </NavLink>
+                <NavLink
+                  exact
+                  to="/"
+                  activeClassName="PageSwitcher__Item--Active"
+                  className="PageSwitcher__Item"
+                >
+                  Sign Up
+                </NavLink>
+              </div>
+
+              <div className="FormTitle">
+                <NavLink
+                  to="/sign-in"
+                  activeClassName="FormTitle__Link--Active"
+                  className="FormTitle__Link"
+                >
+                  Sign In
+                </NavLink>{" "}
+                or{" "}
+                <NavLink
+                  exact
+                  to="/"
+                  activeClassName="FormTitle__Link--Active"
+                  className="FormTitle__Link"
+                >
+                  Sign Up
+                </NavLink>
+              </div>
+
+              {/* <Route exact path="/" component={SignUpForm}></Route> */}
+              <Route path="/sign-in" component={SingnIn}></Route>
+            </div>
+          </div>
         </Switch>
       </BrowserRouter>
     </Fragment>
   );
 }
-
 
 export default App;
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,18 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 import Header from "./header";
 import Footer from "./footer";
+import SignIn from "./SignIn";
 const HomePage = () => {
-	return(
-		<div>
-
-		<h1>
-			HomePage
-		</h1>
-	
-		</div>
-		
-	)
+  return (
+    <div>
+      <h1>HomePage</h1>
+    </div>
+  );
 };
 
 export default HomePage;
