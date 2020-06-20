d427f3b1bd47727a652641aa2323ba1ca8b32b14
SRINIVASAN RAJAGOPAL <srinivasanrajagopal00@gmail.com>
2020-06-20 10:39:14 +0000

    Home Page

--- client/src/pages/footer.js
+++ client/src/pages/footer.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 function Footer() {
-  return (
-    <div>
-      <h1>This is footer</h1>
+return(
+<footer>
+    <div class="container">
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="https://nowmozilla.club/">nowmozilla.club</a>
     </div>
-  );
-}
+	</div>
+</footer>
+
+);
+};
 
 export default Footer;
--- client/src/pages/header.js
+++ client/src/pages/header.js
@@ -2,18 +2,24 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function Header() {
-  return (
-    <nav>
-      <NavLink exact activeClassName="active" to="/">
-        Home
-      </NavLink>
-      <NavLink activeClassName="active" to="/users">
-        Users
-      </NavLink>
-      <NavLink activeClassName="active" to="/contact">
-        Contact
-      </NavLink>
-    </nav>
-  );
-}
+return (
+<header>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
+    <div class="wrapper">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">CoursePlace</a>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="home">HOME <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link active" href="login">LOGIN</a>
+                    <a class="nav-item nav-link active" href="sign up">SIGN UP</a>
+                </div>
+            </div>
+        </nav>
+
+    </div>
+</header>
+
+);
+};
 export default Header;
